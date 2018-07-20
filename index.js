36b377f345f01387ce56bb470451c196fa007d3f
Ruby Reilly <rubyclaroreilly@gmail.com>
2018-07-20 22:48:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var recipes={}
+var recipes={};
 
 function updateObjectWithKeyAndValue(object, key, value){
   var newObj= Object.assign({},obj);
-  return newObj
+  return newObj;
   
 }
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key]=value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key){
