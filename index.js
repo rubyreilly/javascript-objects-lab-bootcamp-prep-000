7b8f9aaca9cb0e9ba3c46101ca234a6a8653a6ea
Ruby Reilly <rubyclaroreilly@gmail.com>
2018-07-20 22:53:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
+  var newObj= Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+  
   
 }
 
